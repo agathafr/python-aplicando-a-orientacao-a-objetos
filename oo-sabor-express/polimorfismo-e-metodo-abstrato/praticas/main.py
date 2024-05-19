6db899a3ba56f999a0b35928b34998a5dce49515
Agatha <85461130+agathafr@users.noreply.github.com>
2024-05-19 16:32:18 -0300

    Create main.py

---
+++ oo-sabor-express/polimorfismo-e-metodo-abstrato/praticas/main.py
@@ -0,0 +1,14 @@
+'''
+Em um arquivo chamado main.py, importe a classe Carro.
+No arquivo main.py, instancie três objetos da classe Carro com diferentes características, como marca, modelo e cor.
+'''
+
+from carro import Carro
+
+carro_01 = Carro('Toyota', 'Corolla', 4, 'Azul Petrólio')
+carro_02 = Carro('Honda', 'Civic', 4, 'Verde')
+carro_03 = Carro('Ford', 'Mustang', 2, 'índigo')
+
+print(carro_01)
+print(carro_02)
+print(carro_03)
