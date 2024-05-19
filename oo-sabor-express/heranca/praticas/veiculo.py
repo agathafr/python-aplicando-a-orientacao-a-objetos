eac159c337f4e5662529624ce58e1e82302d4dc2
Agatha <85461130+agathafr@users.noreply.github.com>
2024-05-19 14:17:32 -0300

    Create veiculo.py

---
+++ oo-sabor-express/heranca/praticas/veiculo.py
@@ -0,0 +1,15 @@
+'''
+- Crie uma Classe Pai (Veiculo): Implemente uma classe chamada Veiculo com um construtor que aceita dois parâmetros, marca e modelo. A classe deve ter um atributo protegido _ligado inicializado como False por padrão.
+- Construa o Método Especial str: Adicione um método especial str à classe Veiculo que retorna uma mensagem formatada com a marca, modelo e o estado de ligado/desligado do veículo.
+'''
+
+class Veiculo:
+    def __init__(self, marca, modelo):
+        self.marca = marca
+        self.modelo = modelo
+        self._ligado = False
+    
+    def __str__(self):
+        status = "ligado" if self._ligado else "desligado"
+        return f'{self.marca.ljust(20)} | {self.modelo.ljust(20)} | {status}'
+    
