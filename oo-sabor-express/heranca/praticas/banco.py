fba7f6deadfb53b3a80fa38cab1935c90a3986d4
Agatha <85461130+agathafr@users.noreply.github.com>
2024-05-19 14:21:03 -0300

    Update banco.py

--- oo-sabor-express/heranca/praticas/banco.py
+++ oo-sabor-express/heranca/praticas/banco.py
@@ -1,3 +1,7 @@
+'''
+Com base no que vimos nessa aula sobre herança, crie uma classe Banco com dois atributos: nome e endereco.
+'''
+
 class Banco:
     def __init__(self, nome, endereco):
         self._nome = nome
